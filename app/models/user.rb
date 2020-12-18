116b5f5dcf13b8c35ecfdcf19b01a32ed52de03d
chockty <75976150+chockty@users.noreply.github.com>
2020-12-18 12:31:19 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :name, presence: true
+  #email/passwordのバリデーションはdeviseによって初期設定されている。 
 end
