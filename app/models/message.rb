6138bca9ce4f6aed37fa6b92bf2d69450880e315
chockty <75976150+chockty@users.noreply.github.com>
2020-12-19 13:19:22 +0900

    画像表示の実装

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,7 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :room
-  validates :content, presence: true
   has_one_attached :image
+  
+  validates :content, presence: true
 end
